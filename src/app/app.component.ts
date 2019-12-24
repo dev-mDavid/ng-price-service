acf452e3c89cf8e1b06445d4d02831ea70586b4e
mDavid <dev.mdavid@gmail.com>
2019-12-24 11:20:54 -0800

    test MockPriceService in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { PriceService } from './price-service-demo/price-service-1';
-import { Product } from './price-service-demo/product-model-1';
+// import { PriceService } from './price-service-demo/price-service-1';
+// import { Product } from './price-service-demo/product-model-1';
+import { MockPriceService } from './price-service-demo/price-service-mock';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'ng-price-service';
 
+  
 }
+console.log(
+  MockPriceService.prototype.calculateTotalPrice(2, 'FL')
+);
 // PriceService.prototype.calculateTotalPrice(27,'florida');
+// Product.prototype.totalPrice('florida')
 
-Product.prototype.totalPrice('florida')
