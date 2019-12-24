533daf37750243f4860497a911ee03b3b7ce6c5e
mDavid <dev.mdavid@gmail.com>
2019-12-24 10:26:28 -0800

    delete all non-boiler plate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PriceServiceDemoComponent } from './price-service-demo/price-service-demo.component';
-import { ProductModelComponent } from './product-model/product-model.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PriceServiceDemoComponent,
-    ProductModelComponent
   ],
   imports: [
     BrowserModule,
--- src/app/price-service-class.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PriceServiceClass } from './price-service-class';
-
-describe('PriceServiceClass', () => {
-  it('should create an instance', () => {
-    expect(new PriceServiceClass()).toBeTruthy();
-  });
-});
