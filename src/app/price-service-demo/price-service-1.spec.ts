8217a8d6007a2ce456309cafe421d5a587f7005c
mDavid <dev.mdavid@gmail.com>
2019-12-24 10:44:37 -0800

    generate PriceService1Class in PriceServiceDemoModule

---
+++ src/app/price-service-demo/price-service-1.spec.ts
@@ -0,0 +1,7 @@
+import { PriceService } from './price-service-1';
+
+describe('PriceService1', () => {
+  it('should create an instance', () => {
+    expect(new PriceService()).toBeTruthy();
+  });
+});
