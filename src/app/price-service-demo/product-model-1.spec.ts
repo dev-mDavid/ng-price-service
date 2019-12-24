d1d4850e7b37c51fd3e2dd5d1647838316b3dda3
mDavid <dev.mdavid@gmail.com>
2019-12-24 10:57:33 -0800

    generate ProductModel1Class in PriceServiceDemoModule

---
+++ src/app/price-service-demo/product-model-1.spec.ts
@@ -0,0 +1,7 @@
+import { ProductModel1 } from './product-model-1';
+
+describe('ProductModel1', () => {
+  it('should create an instance', () => {
+    expect(new ProductModel1()).toBeTruthy();
+  });
+});
