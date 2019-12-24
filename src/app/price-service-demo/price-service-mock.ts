e4e456ae43cf182d7d8e04f4fd8a1119facabcf6
mDavid <dev.mdavid@gmail.com>
2019-12-24 11:18:00 -0800

    implement direct injection of PriceServiceInterface into MockPriceService

--- src/app/price-service-demo/price-service-mock.ts
+++ src/app/price-service-demo/price-service-mock.ts
@@ -1,2 +1,17 @@
-export class PriceServiceMock {
+import { PriceServiceInterface } from './price-service-interface';
+
+export class MockPriceService implements PriceServiceInterface{
+  calculateTotalPrice(
+    basePrice: number,
+    state: string
+  ) {
+    if (state === 'FL') {
+      return basePrice + 0.66;
+    }
+    return basePrice;
+  }
+
+
+
+
 }
